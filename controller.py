818e035cc776ec8323a2a284565586a317dafcfa
Uzair Khan <never.regretonthechoiceyoumade@gmail.com>
2026-03-06 23:21:25 +0500

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,16 +1,17 @@
 from sql_injection import SQLInjectionCheck
 
 class InspectionController:
-    def __init__(self, base_url):
+    def __init__(self, base_url): # This function takes the target application
         self.base_url = base_url
         self.checks = []
 
-    def register_check(self, check):
+    def register_check(self, check): # It adds different security tests 
         self.checks.append(check)
 
-    def run(self):
+    def run(self): # Runs those tests and return results
         results = []
         for check in self.checks:
             result = check.run(self.base_url)
             results.append(result)
         return results
+
